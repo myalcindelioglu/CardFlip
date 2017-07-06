7b79298cd8fd7b34496a811388622248e97e3b29
ttmdelioglu <yalcin.delioglu@turkcell.com.tr>
2017-07-06 08:35:31 +0300

    Initial commit

---
+++ app/src/main/java/com/myd/cardflip/CardBackFragment.java
@@ -0,0 +1,20 @@
+package com.myd.cardflip;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by turkcell on 7/5/17.
+ */
+
+public class CardBackFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_card_back, container, false);
+    }
+}
---
+++ app/src/main/java/com/myd/cardflip/MainActivity.java
@@ -0,0 +1,62 @@
+package com.myd.cardflip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    boolean showingBack = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FrameLayout frameLayout = (FrameLayout)findViewById(R.id.container);
+        frameLayout.setOnClickListener(this);
+
+        if (savedInstanceState == null) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new CardFrontFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.container:
+                flipCard();
+                break;
+            default:
+                Log.e(TAG, "Unhandled onclick event for view id: " + v.getId());
+                break;
+        }
+    }
+
+    private void flipCard() {
+        if (showingBack) {
+            showingBack = false;
+            getFragmentManager().popBackStack();
+        } else {
+            showingBack = true;
+            getFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(
+                            R.animator.card_flip_right_enter,
+                            R.animator.card_flip_right_exit,
+                            R.animator.card_flip_left_enter,
+                            R.animator.card_flip_left_exit)
+                    .addToBackStack(null)
+                    .replace(R.id.container, new CardBackFragment())
+                    .commit();
+        }
+    }
+}
